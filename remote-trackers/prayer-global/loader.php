eafe70c6de60944faaa3798a9e5e3b6db175776f
Chris Chasm <chris@chasm.solutions>
2024-11-01 14:05:04 -0600

    restructuring for multi-use

---
+++ remote-trackers/prayer-global/loader.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Auto loads 2 levels of php files, and file in subdirectories
+ */
+
+// Load dependencies first
+
+$skip_folders = [];
+
+// Load all other files
+$dir = scandir( __DIR__ );
+foreach ( $dir as $file ){
+    if ( 'php' === substr( $file, -3, 3 ) && 'index.php' !== $file && 'loader.php' !== $file && substr( $file, 0, 1 ) !== '.' ) {
+        require_once( __DIR__ . '/' . $file );
+    }
+    if ( 'index.php' !== $file && 'loader.php' !== $file && substr( $file, 0, 1 ) !== '.' && is_dir( __DIR__ . '/' . $file ) && !in_array( $file, $skip_folders ) ) {
+        $subdir = scandir( __DIR__ . '/'. $file );
+        foreach ( $subdir as $subfile ){
+            if ( in_array( $subfile, $skip_folders ) ) {
+                continue;
+            }
+            if ( 'php' === substr( $subfile, -3, 3 ) && 'index.php' !== $subfile && 'loader.php' !== $subfile && substr( $subfile, 0, 1 ) !== '.' ) {
+                require_once( __DIR__ . '/'. $file . '/' . $subfile );
+            }
+            if ( substr( $subfile, 0, 1 ) !== '.' && is_dir( __DIR__ . '/'. $file . '/' . $subfile ) ) {
+                $subsubdir = scandir( __DIR__ . '/'. $file . '/' . $subfile );
+                foreach ( $subsubdir as $subsubfile ){
+                    if ( in_array( $subsubfile, $skip_folders ) ) {
+                        continue;
+                    }
+                    if ( 'php' === substr( $subsubfile, -3, 3 ) && 'index.php' !== $subsubfile && 'loader.php' !== $subsubfile && substr( $subsubfile, 0, 1 ) !== '.' ) {
+                        require_once( __DIR__ . '/'. $file . '/' . $subfile . '/' . $subsubfile );
+                    }
+                }
+            }
+        }
+    }
+}
