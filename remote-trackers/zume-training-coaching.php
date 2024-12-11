2c660639b18f683a6a19e4dec5525e7569c520fe
Chris Chasm <chris@chasm.solutions>
2024-12-11 07:02:22 -0700

    coaching edits

--- remote-trackers/zume-training-coaching.php
+++ remote-trackers/zume-training-coaching.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 
 /**
@@ -22,3 +22,35 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 //         ] );
 //     }
 // }, 10, 4 );
+
+add_action('zume_log', function( $args ) {
+    if ( in_array( $args['type'], ['system'] ) ) {
+        return;
+    }
+
+    add_log_to_queue( [
+        'post_type' => 'zume',
+        'type' => $args['type'],
+        'subtype' => $args['subtype'],
+        'time' => time(),
+        'language_code' => get_locale() ?? $args['language_code'] ?? 'en',
+        'location' => [
+            'ip' => get_ip_address_for_log()
+        ],
+    ] );
+
+}, 10, 1 );
+
+add_action('zume_log_anonymous', function( $args ) {
+    add_log_to_queue( [
+        'post_type' => 'zume',
+        'type' => $args['type'],
+        'subtype' => $args['subtype'],
+        'time' => time(),
+        'language_code' => get_locale() ?? $args['language_code'] ?? 'en',
+        'location' => [
+            'ip' => get_ip_address_for_log()
+        ],
+    ] );
+
+}, 10, 1 );
--- remote-trackers/zume-training.php
+++ remote-trackers/zume-training.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 
 add_action('zume_log', function( $args ) {
@@ -12,18 +12,10 @@ add_action('zume_log', function( $args ) {
         'subtype' => $args['subtype'],
         'time' => time(),
         'language_code' => get_locale() ?? $args['language_code'] ?? 'en',
-        'location' => [ 'ip' => get_ip_address_for_log() ],
+        'location' => [
+            'ip' => get_ip_address_for_log()
+        ],
     ] );
-
-    // dt_write_log('zume_log');
-    // dt_write_log( [
-    //     'post_type' => 'zume',
-    //     'type' => $args['type'],
-    //     'subtype' => $args['subtype'],
-    //     'time' => time(),
-    //     'language_code' => get_locale() ?? $args['language_code'] ?? 'en',
-    //     'location' => [ 'ip' => get_ip_address_for_log() ],
-    // ]  );
 
 }, 10, 1 );
 
@@ -34,17 +26,9 @@ add_action('zume_log_anonymous', function( $args ) {
         'subtype' => $args['subtype'],
         'time' => time(),
         'language_code' => get_locale() ?? $args['language_code'] ?? 'en',
-        'location' => [ 'ip' => get_ip_address_for_log() ],
+        'location' => [
+            'ip' => get_ip_address_for_log()
+        ],
     ] );
-
-    // dt_write_log('zume_log_anonymous');
-    // dt_write_log( [
-    //     'post_type' => 'zume',
-    //     'type' => $args['type'],
-    //     'subtype' => $args['subtype'],
-    //     'time' => time(),
-    //     'language_code' => get_locale() ?? $args['language_code'] ?? 'en',
-    //     'location' => [ 'ip' => get_ip_address_for_log() ],
-    // ]  );
 
 }, 10, 1 );
