79e7f0c1496c3e6bd1318381fc651f25cd8da29d
Chris Chasm <chris@chasm.solutions>
2025-01-04 13:23:16 -0700

    v0.7

--- gospel-ambition-impact-map.php
+++ gospel-ambition-impact-map.php
@@ -5,7 +5,7 @@
  * Description: Gospel Ambition - Impact Map shows some of the global activity of the GO digital disciple making activity.
  * Text Domain: gospel-ambition-impact-map
  * Domain Path: /languages
- * Version:  0.6
+ * Version:  0.7
  * Author URI: https://github.com/GospelAmbition
  * GitHub Plugin URI: https://github.com/GospelAmbition/gospel-ambition-impact-map
  * Requires at least: 4.7.0
@@ -67,3 +67,66 @@ class GO_Impact_Context_Switcher {
 }
 
 add_action( 'after_setup_theme', [ 'GO_Impact_Context_Switcher', 'instance' ], 10 );
+
+if ( ! function_exists( 'ga_impact_map_hook_admin_notice' ) ) {
+    function ga_impact_map_hook_admin_notice() {
+        global $ga_impact_map_required_dt_theme_version;
+        $wp_theme = wp_get_theme();
+        $current_version = $wp_theme->version;
+        $message = "'Disciple.Tools - Gospel Ambition Impact Map' plugin requires 'Disciple.Tools' theme to work. Please activate 'Disciple.Tools' theme or make sure it is latest version.";
+        if ( $wp_theme->get_template() === 'disciple-tools-theme' ){
+            $message .= ' ' . sprintf( esc_html( 'Current Disciple.Tools version: %1$s, required version: %2$s' ), esc_html( $current_version ), esc_html( $ga_impact_map_required_dt_theme_version ) );
+        }
+        // Check if it's been dismissed...
+        if ( ! get_option( 'dismissed-gospel-ambition-impact-map', false ) ) { ?>
+            <div class="notice notice-error notice-gospel-ambition-impact-map is-dismissible" data-notice="gospel-ambition-impact-map">
+                <p><?php echo esc_html( $message );?></p>
+            </div>
+            <script>
+                jQuery(function($) {
+                    $( document ).on( 'click', '.notice-gospel-ambition-impact-map .notice-dismiss', function () {
+                        $.ajax( ajaxurl, {
+                            type: 'POST',
+                            data: {
+                                action: 'dismissed_notice_handler',
+                                type: 'gospel-ambition-impact-map',
+                                security: '<?php echo esc_html( wp_create_nonce( 'wp_rest_dismiss' ) ) ?>'
+                            }
+                        })
+                    });
+                });
+            </script>
+        <?php }
+    }
+}
+
+/**
+ * AJAX handler to store the state of dismissible notices.
+ */
+if ( !function_exists( 'dt_hook_ajax_notice_handler' ) ){
+    function dt_hook_ajax_notice_handler(){
+        check_ajax_referer( 'wp_rest_dismiss', 'security' );
+        if ( isset( $_POST['type'] ) ){
+            $type = sanitize_text_field( wp_unslash( $_POST['type'] ) );
+            update_option( 'dismissed-' . $type, true );
+        }
+    }
+}
+
+add_action( 'plugins_loaded', function (){
+    if ( ( is_admin() && !( is_multisite() && class_exists( 'DT_Multisite' ) ) ) || ( wp_doing_cron() ) ) {
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' ) ) {
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ){
+            Puc_v4_Factory::buildUpdateChecker(
+                'https://raw.githubusercontent.com/GospelAmbition/gospel-ambition-impact-map/master/version-control.json',
+                __FILE__,
+                'gospel-ambition-impact-map'
+            );
+        }
+    }
+} );
--- impact-map/loader.php
+++ impact-map/loader.php
@@ -184,69 +184,7 @@ register_activation_hook( __FILE__, [ 'GO_Impact_Map', 'activation' ] );
 register_deactivation_hook( __FILE__, [ 'GO_Impact_Map', 'deactivation' ] );
 
 
-if ( ! function_exists( 'ga_impact_map_hook_admin_notice' ) ) {
-    function ga_impact_map_hook_admin_notice() {
-        global $ga_impact_map_required_dt_theme_version;
-        $wp_theme = wp_get_theme();
-        $current_version = $wp_theme->version;
-        $message = "'Disciple.Tools - Gospel Ambition Impact Map' plugin requires 'Disciple.Tools' theme to work. Please activate 'Disciple.Tools' theme or make sure it is latest version.";
-        if ( $wp_theme->get_template() === 'disciple-tools-theme' ){
-            $message .= ' ' . sprintf( esc_html( 'Current Disciple.Tools version: %1$s, required version: %2$s' ), esc_html( $current_version ), esc_html( $ga_impact_map_required_dt_theme_version ) );
-        }
-        // Check if it's been dismissed...
-        if ( ! get_option( 'dismissed-gospel-ambition-impact-map', false ) ) { ?>
-            <div class="notice notice-error notice-gospel-ambition-impact-map is-dismissible" data-notice="gospel-ambition-impact-map">
-                <p><?php echo esc_html( $message );?></p>
-            </div>
-            <script>
-                jQuery(function($) {
-                    $( document ).on( 'click', '.notice-gospel-ambition-impact-map .notice-dismiss', function () {
-                        $.ajax( ajaxurl, {
-                            type: 'POST',
-                            data: {
-                                action: 'dismissed_notice_handler',
-                                type: 'gospel-ambition-impact-map',
-                                security: '<?php echo esc_html( wp_create_nonce( 'wp_rest_dismiss' ) ) ?>'
-                            }
-                        })
-                    });
-                });
-            </script>
-        <?php }
-    }
-}
 
-/**
- * AJAX handler to store the state of dismissible notices.
- */
-if ( !function_exists( 'dt_hook_ajax_notice_handler' ) ){
-    function dt_hook_ajax_notice_handler(){
-        check_ajax_referer( 'wp_rest_dismiss', 'security' );
-        if ( isset( $_POST['type'] ) ){
-            $type = sanitize_text_field( wp_unslash( $_POST['type'] ) );
-            update_option( 'dismissed-' . $type, true );
-        }
-    }
-}
-
-add_action( 'plugins_loaded', function (){
-    if ( ( is_admin() && !( is_multisite() && class_exists( 'DT_Multisite' ) ) ) || ( wp_doing_cron() ) ) {
-        // Check for plugin updates
-        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' ) ) {
-                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-            }
-        }
-        if ( class_exists( 'Puc_v4_Factory' ) ){
-            Puc_v4_Factory::buildUpdateChecker(
-                'https://raw.githubusercontent.com/GospelAmbition/gospel-ambition-impact-map/master/version-control.json',
-                __FILE__,
-                'gospel-ambition-impact-map'
-            );
-
-        }
-    }
-} );
 
 if ( ! function_exists( 'zume_languages' ) ) {
     /**
